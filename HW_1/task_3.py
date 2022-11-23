7e51e1d3fc78b3900c1b1cc4f138b115f969cf2c
Tarutko Oleg <Skyli2011@gmail.com>
2022-11-23 14:23:09 +0300

    Task3 complete

--- HW_1/task_3.py
+++ HW_1/task_3.py
@@ -19,3 +19,21 @@
 0
 Точка на отрицательной части оси абсцисс
 """
+x = float(input('Enter X coordinate: '))
+y = float(input('Enter Y coordinate: '))
+if x > 0 and y > 0:
+    print('first quarter')
+elif x < 0 and y > 0:
+    print('second quarter')
+elif x < 0 and y < 0:
+    print('third quarter')
+elif x > 0 and y < 0:
+    print('fourth quarter')
+elif x == 0 and y < 0:
+    print('Point on the negative part of the y-axis')
+elif x == 0 and y > 0:
+    print('Point on the positive part of the y-axis')
+elif x > 0 and y == 0:
+    print('Point on the positive part of the x-axis')
+elif x < 0 and y == 0:
+    print('Point on the negative part of the x-axis')

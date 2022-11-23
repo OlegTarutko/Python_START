d1d94ce7e84813e2dc2d6f8ff3bec7a0a50b7ddf
Tarutko Oleg <Skyli2011@gmail.com>
2022-11-23 14:25:29 +0300

    Task5 complete

--- HW_1/task_5.py
+++ HW_1/task_5.py
@@ -11,3 +11,10 @@
 11 5
 9.22
 """
+x1 = int(input("Enter x1 coordinate: "))
+y1 = int(input("Enter y1 coordinate: "))
+x2 = int(input("Enter x2 coordinate: "))
+y2 = int(input("Enter y2 coordinate: "))
+
+S = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+print(round(S, 2))

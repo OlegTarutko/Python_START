8004fc22d677d66e7babce4aa29186cf31f147fa
Tarutko Oleg <Skyli2011@gmail.com>
2022-11-27 18:35:14 +0300

    complete task 4

--- HW_2/task_4.py
+++ HW_2/task_4.py
@@ -6,3 +6,26 @@
 Ввод: значение типа <int>
 Вывод: значение типа <int>
 """
+# path = 'indexes.txt'
+# data = open(path, 'r')
+# for line in data:
+#     print(line)
+# data.close()
+N = int(input('Enter N: '))
+import random
+
+rnd_list = []
+for i in range(0, N):
+    rnd_list.append(random.randint(-N, N))
+# print(rnd_list)
+
+with open("indexes.txt") as data: index_list = [int(line.strip()) for line in data]
+# print(index_list)
+
+mult = 1
+for i in range(len(rnd_list)):
+    if i in index_list:
+        mult *= rnd_list[i]
+    if -i in index_list and i != 0:
+        mult *= rnd_list[-i]
+print(mult)

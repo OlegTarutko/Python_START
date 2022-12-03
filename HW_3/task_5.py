799b5fad6ce6fcb3f42154e5496ed4f9f00e23a6
Tarutko Oleg <Skyli2011@gmail.com>
2022-12-03 11:51:10 +0300

    complete task5

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -10,3 +10,12 @@ https://ru.wikipedia.org/wiki/Негафибоначчи
 8
 [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 """
+n = int(input("Enter number: "))
+fib_lst = [0] * (2 * n + 1)
+fib_lst[n + 1], fib_lst[n - 1] = 1, 1
+k = -1
+for i in range(n + 2, 2 * n + 1):
+    fib_lst[i] = fib_lst[i - 1] + fib_lst[i - 2]
+    fib_lst[-i - 1] = fib_lst[i] * k
+    k *= -1
+print(fib_lst)

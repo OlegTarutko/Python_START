bb21a3530be854c32c63d849ee29b6b0f3d1f6cd
Tarutko Oleg <Skyli2011@gmail.com>
2022-12-03 12:20:18 +0300

    complete task3

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,15 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+import random
+
+number = int(input("Enter number: "))
+lst = [round(random.uniform(1, 9), 2) for i in range(number)]
+lst_2 = []
+
+for i in range(len(lst)):
+    lst_2.append(lst[i] - int(lst[i]))
+result = max(lst_2) - min(lst_2)
+
+print(lst)  # Для проверки работоспособности.
+print(round(result, 2))

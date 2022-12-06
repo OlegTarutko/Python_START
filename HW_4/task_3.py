b850c380d71be32a2006cb35b75cf157c10a04bc
Tarutko Oleg <Skyli2011@gmail.com>
2022-12-06 13:52:18 +0300

    create polynomial task3

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -10,3 +10,29 @@
 2
 2x^2 + 4x + 5 = 0
 """
+from random import randint
+from os import path, mkdir, chdir
+
+k = int(input('Enter polynomial degree: '))
+lst = [randint(0, 100) for number in range(k)]
+
+
+def new_polynomial(lst):
+    if sum(lst) == 0:
+        return '0=0'
+    i = len(lst) - 1
+    result = []
+    for num in lst:
+        if num:
+            if i == 0:
+                result.append(f'{num}')
+            elif num == 1:
+                result.append(f'{num if num != 1 else ""}x')
+            else:
+                result.append(f'{num if num != 1 else ""}x^{i}')
+        i -= 1
+    return ' + '.join(result) + ' =0'
+
+
+polynomial = new_polynomial(lst)
+print(polynomial)

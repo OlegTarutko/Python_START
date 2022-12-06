3e0649c47aba78fe46e0de290b40a9758047f617
Tarutko Oleg <Skyli2011@gmail.com>
2022-12-06 14:08:31 +0300

    complete task 3

--- HW_4/task_3.py
+++ HW_4/task_3.py
@@ -13,26 +13,39 @@
 from random import randint
 from os import path, mkdir, chdir
 
-k = int(input('Enter polynomial degree: '))
+k = int(input('Enter polynomial degree: '))  # Коэффициент полинома
 lst = [randint(0, 100) for number in range(k)]
 
 
-def new_polynomial(lst):
-    if sum(lst) == 0:
+def new_polynomial(lst):  # создание полинома степени К
+    if sum(lst) == 0:  # Проверка на 0
         return '0=0'
     i = len(lst) - 1
     result = []
     for num in lst:
         if num:
             if i == 0:
-                result.append(f'{num}')
+                result.append(f'{num}')  # значение без переменной
             elif num == 1:
-                result.append(f'{num if num != 1 else ""}x')
+                result.append(f'{num if num != 1 else ""}x')  # Без 1
             else:
-                result.append(f'{num if num != 1 else ""}x^{i}')
+                result.append(f'{num if num != 1 else ""}x^{i}')  # степени
         i -= 1
     return ' + '.join(result) + ' =0'
 
 
-polynomial = new_polynomial(lst)
+polynomial = new_polynomial(lst)  # Вызов функции создания полинома
 print(polynomial)
+
+if not path.isdir("polynomial"):  # Проверка наличия файла с текущим именем и создание нового.
+    mkdir("polynomial")  # Создание папки
+chdir("polynomial")  # проверка файла
+i = 0
+while True:
+    if not path.isfile(f"polynomial_{i}.txt"):
+        with open(f"polynomial_{i}.txt", 'w') as file:
+            file.write(polynomial)
+            print(f"polynomial written to file polynomial_{i}.txt")
+        break
+    else:
+        i += 1

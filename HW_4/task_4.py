e3e74cbde6305f107986bf95272de17f2bb6bc52
Tarutko Oleg <Skyli2011@gmail.com>
2022-12-07 16:26:45 +0300

    complete task 4, correction task 3, add message

--- HW_4/task_4.py
+++ HW_4/task_4.py
@@ -11,3 +11,36 @@
 3x^2+2x+1=0
 9x^5+7x^4+7x^3+12x^2+8x+18=0
 """
+from sympy.abc import x
+from sympy import *
+from os import path, chdir, listdir
+
+if path.isdir("polynomial"):  # Проверка наличия папки
+    chdir("polynomial")
+else:
+    print("Каталог с файлами отсутствует")
+    exit()
+
+lst_polynomial = []  # Создание пустого списка.
+for file_name in listdir():
+    if "polynomial_" in file_name:  # Проверка наличия файлов.
+        with open(file_name, 'r') as file:
+            str_polynomial = file.read()
+            print(f"{str_polynomial} from file {file_name}")
+            lst_polynomial.append(str_polynomial)
+print(lst_polynomial)
+
+
+def sum_polynomial(*args):
+    res = []
+    for arg in args:
+        s = arg.replace('x', ' * x').replace('x^', 'x ** ').replace(' = 0', '')  # Перевод на Пайтон язык
+        res.append(collect(s, x))
+    return str(sum(res)).replace('*x', 'x').replace('**', '^') + ' = 0'  # Перевод обратно в математическую запись.
+
+
+print(sum_polynomial(*lst_polynomial))
+
+with open("sum_polynomial.txt", 'w') as file:  # Запись результата в файл.
+    file.write(sum_polynomial(*lst_polynomial))
+    print("Result is written to a file sum_polynom.txt")

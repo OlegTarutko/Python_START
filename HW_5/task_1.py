36bc8e0367b5d8bb25112b0841d58bfed3d64ec1
Tarutko Oleg <Skyli2011@gmail.com>
2022-12-09 09:02:43 +0300

    task 1 complete

--- HW_5/task_1.py
+++ HW_5/task_1.py
@@ -4,3 +4,12 @@
 Ввод: значение типа <str>
 Вывод: значение типа <str>
 """
+str_text = "забвение аптека компьютер семинар автомобиль"
+letters = "абв"
+str_val = []
+
+value = str_text.split()
+for word in value:
+    if not (letters[0] in word and letters[1] in word and letters[2] in word):
+        str_val.append(word)
+print(" ".join(str_val))
